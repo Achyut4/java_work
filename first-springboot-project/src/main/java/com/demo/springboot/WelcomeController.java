23e311e3b4babe82e1eaaa33b5b390b515c64c2b
Achyut Vyas <atk4298@gmail.com>
2020-10-02 20:36:38 +0530

    Perform Auto Wiring

--- first-springboot-project/src/main/java/com/demo/springboot/WelcomeController.java
+++ first-springboot-project/src/main/java/com/demo/springboot/WelcomeController.java
@@ -1,26 +1,30 @@
 package com.demo.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 	
-	private WelcomeService  service = new WelcomeService();
+	//Auto Wiring
+	@Autowired
+	private WelcomeService service;
 	@RequestMapping("/welcome")
 	public String welcome() {
 		
 		return service.retrieveWlcomeMessage();
 	}
-	
-
 }
 
+// Spring to Manage this bean and Create an Instance of this
+@Component
 class WelcomeService{
 	
 	protected String retrieveWlcomeMessage() {
 		
 		//Complex Method
-		return "Good Morning"; 
+		return "Good Morning Updated"; 
 	}
 }
